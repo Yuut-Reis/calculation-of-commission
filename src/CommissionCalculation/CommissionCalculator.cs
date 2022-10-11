9563620a14025f9fbc40d61499504b1f411a95df
Yuut <ingride.reis.b@gmail.com>
2022-10-11 18:06:10 -0300

    feat(config): :tada: starting project

--- src/CommissionCalculation/CommissionCalculator.cs
+++ src/CommissionCalculation/CommissionCalculator.cs
@@ -4,18 +4,25 @@ namespace CommissionCalculation;
 public class CommissionCalculator
 {
     public const decimal CommissionPerCarSold = 250;
+    public const decimal Percentage = 3/100M;
     public decimal FixedSalary { get; set; }
     public int AmountCarsSold { get; set; }
     public decimal TotalSalesValue { get; set; }
     public decimal FinalSalary { get; set; }
-
+    public decimal PercentageBySales { get; set;}
     public void CalculateFinalSalary(decimal fixedSalary, int amountCarsSold, decimal totalSalesValue) 
     {
-        throw new NotImplementedException();
+        AmountCarsSold = amountCarsSold;
+        TotalSalesValue = totalSalesValue;
+        FixedSalary = fixedSalary;
+        FinalSalary =  FixedSalary + (AmountCarsSold * CommissionPerCarSold) + (TotalSalesValue * Percentage);
+        PercentageBySales = TotalSalesValue * Percentage;
+
     }
 
     public void ShowFinalSalary(string contributorName, string month)
     {
-        throw new NotImplementedException();
+      throw new NotImplementedException();  
     }
+
 }
